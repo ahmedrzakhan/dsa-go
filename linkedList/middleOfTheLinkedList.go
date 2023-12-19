a153b4a8cef71ec809d4a481591c54df0ad71ea5
Ahmed Khan <ahmed.khan@smallcase.com>
2023-12-19 21:02:45 +0000

    name

--- linkedList/middleOfTheLinkedList.go
+++ linkedList/middleOfTheLinkedList.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 /**
+876. Middle of the Linked List
+
 Given the head of a singly linked list, return the middle node of the linked list.
 
 If there are two middle nodes, return the second middle node.
