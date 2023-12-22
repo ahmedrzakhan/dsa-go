d40def48254fc425fc501c4a31c353b9aceca42f
Ahmed Khan <ahmed.khan@smallcase.com>
2023-12-22 15:26:12 +0000

    binary search

---
+++ binarySearch/binarySearch.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+704. Binary Search
+
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+*/
+
+// NOTE: L + R, if they are 2^32-1 + huge digit it would fail
+// mid = L + (R - L) / 2
+
+// TC - O(LogN), SC - O(1)
+func search(nums []int, target int) int {
+	L := 0
+	R := len(nums) - 1
+
+	for L <= R {
+		mid := (L + R) / 2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			L = mid + 1
+		} else {
+			R = mid - 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	fmt.Println(search(nums, 9))
+}
