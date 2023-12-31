df4df7ffc64227d81c578a91c2e203e3508b6ae5
Ahmed Khan <ahmed.khan@smallcase.com>
2023-12-31 21:49:56 +0000

    max height

---
+++ recursion/basics/maxHeight.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	} else {
+		return rightDepth + 1
+	}
+}
+
+func mainMaxHeight() {
+	// Example Tree
+	//        3
+	//       / \
+	//      9  20
+	//        /  \
+	//       15   7
+	root := &TreeNode{3, &TreeNode{9, nil, nil}, &TreeNode{20, &TreeNode{15, nil, nil}, &TreeNode{7, nil, nil}}}
+	fmt.Println("Maximum depth of the tree is:", maxDepth(root))
+}
