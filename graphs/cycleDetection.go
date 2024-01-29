2e0175d267b93dbbde241296738991c3c52355b5
Ahmed Khan <ahmed.khan@smallcase.com>
2024-01-29 18:44:59 +0000

    visiting

--- graphs/cycleDetection.go
+++ graphs/cycleDetection.go
@@ -6,7 +6,7 @@ import "fmt"
 func detectCycle(edges [][]int) bool {
 	adjList := make(map[int][]int)
 	visited := make(map[int]bool)
-	recStack := make(map[int]bool)
+	visiting := make(map[int]bool)
 
 	for _, edge := range edges {
 		src, dst := edge[0], edge[1]
@@ -17,7 +17,7 @@ func detectCycle(edges [][]int) bool {
 
 	for node := range adjList {
 		if !visited[node] {
-			if dfsCD(node, adjList, visited, recStack) {
+			if dfsCD(node, adjList, visited, visiting) {
 				return true
 			}
 		}
@@ -26,8 +26,8 @@ func detectCycle(edges [][]int) bool {
 	return false
 }
 
-func dfsCD(node int, adjList map[int][]int, visited, recStack map[int]bool) bool {
-	if recStack[node] {
+func dfsCD(node int, adjList map[int][]int, visited, visiting map[int]bool) bool {
+	if visiting[node] {
 		return true // Cycle detected
 	}
 
@@ -36,15 +36,15 @@ func dfsCD(node int, adjList map[int][]int, visited, recStack map[int]bool) bool
 	}
 
 	visited[node] = true
-	recStack[node] = true
+	visiting[node] = true
 
 	for _, neighbor := range adjList[node] {
-		if dfsCD(neighbor, adjList, visited, recStack) {
+		if dfsCD(neighbor, adjList, visited, visiting) {
 			return true
 		}
 	}
 
-	recStack[node] = false // Remove from recursion stack when backtracking
+	visiting[node] = false // Remove from recursion stack when backtracking
 	return false
 }
 
