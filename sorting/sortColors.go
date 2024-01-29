0f7a38d6b0ede04bed382a819c0394e5d66864fa
Ahmed Khan <ahmed.khan@smallcase.com>
2024-01-29 16:16:17 +0000

    sort colors

---
+++ sorting/sortColors.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+/**
+75. Sort Colors
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects
+of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+*/
+
+// TC - O(N), SC - O(1)
+func sortColors(arr []int) []int {
+	counts := make([]int, 3)
+
+	for _, val := range arr {
+		counts[val]++
+	}
+
+	i := 0
+	for idx, val := range counts {
+		for j := 0; j < val; j++ {
+			arr[i] = idx
+			i++
+		}
+	}
+
+	return arr
+}
+
+func mainSC() {
+	arr := []int{2, 0, 2, 1, 1, 0}
+	fmt.Println(sortColors(arr))
+}
