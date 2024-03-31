c6eede465f8a4a706d4b0dcf5df13e24f88e0785
Ahmed Khan <ahmed.khan@smallcase.com>
2024-03-31 14:29:51 +0000

    valid palindrome

---
+++ arrays/twoPointers/validPalindrome.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+/**
+125. Valid Palindrome
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+*/
+
+func isPalindrome(s string) bool {
+	var cleanedStr strings.Builder
+	for _, ch := range s {
+		if unicode.IsLetter(ch) || unicode.IsNumber(ch) {
+			cleanedStr.WriteRune(unicode.ToLower(ch))
+		}
+	}
+
+	cleanedString := cleanedStr.String() // Get the accumulated string
+	lengthOfStr := len(cleanedString)    // Get the length of the string
+
+	for i := 0; i < lengthOfStr/2; i++ {
+		if cleanedString[i] != cleanedString[lengthOfStr-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
